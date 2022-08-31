3094c15bd398537578234100b3cfdfc610bc3440
matheusfillipe <matheusfillipeag@gmail.com>
2022-08-31 19:58:25 -0300

    fix cli

--- chartlyrics/__main__.py
+++ chartlyrics/__main__.py
@@ -6,14 +6,23 @@ import click
 @click.command()
 @click.argument("search_query")
 @click.option('-a', "--artist", default=None, help="Artist name")
-def main(search_query, artist):
+@click.option('-l', "--lyrics", default=None, help="Show lyrics to the nth result")
+def main(search_query, artist, lyrics):
     client = ChartLyricsClient()
-    if artist is None:
-        for song in client.search_text(search_query):
+    if lyrics is None:
+        if artist is None:
+            for song in client.search_text(search_query):
+                print(str(song))
+            return
+        for song in client.search_artist_and_song(artist, search_query):
             print(str(song))
         return
-    for song in client.search_artist_and_song(artist, search_query):
-        print(str(song))
+    i = 0
+    for song in client.search_text(search_query):
+        if i == int(lyrics):
+            print(str(song.lyrics))
+            return
+        i += 1
 
 if __name__ == '__main__':
     main()
